1d17d7ba7978fcfad767d050262e62a44717ce4d
vigkk <viglionigr@gmail.com>
2016-10-09 19:48:37 -0300

    método taça funcionando.

--- new_phil.c
+++ new_phil.c
@@ -6,13 +6,30 @@
 #include <assert.h>
 #include <semaphore.h>
 
+
+// definiçoes
+
 #define REF 50 // número de refeiçoes servidas
 #define FIL 5 // número de filósofos
 
+
+// variáveis globais
+
 int ref_comidas[FIL] = {0,0,0,0,0};
+volatile int taca[2] = {0,1};
+volatile int temTaca[2] = {-1,-1};
+volatile int temGarfo[5] = {-1,-1,-1,-1,-1};
+
+
+// cabeçalhos
 
 void create_phils(pthread_t *phil, int *phil_id);
 void f_thread (void *v);
+void pensa(int phil_id);
+void EsperaTaca(int phil_id);
+void EsperaGarfo(int phil_id);
+
+// Main
 
 int main(){
 
@@ -28,6 +45,20 @@ int main(){
   return 0;
 }
 
+// f_thread
+
+void f_thread (void *v) {
+  int t_id = *(int *) v;
+  printf("Filósofo %d sentou-se a mesa. ʘ‿ʘ \n", t_id);
+
+  pensa(t_id);
+  EsperaTaca(t_id);
+  
+  return NULL;
+}
+
+// outras funçoes
+
 void create_phils(pthread_t *phil, int *phil_id) {
   int i;
   for (i = 0; i < FIL; i++) {
@@ -36,8 +67,26 @@ void create_phils(pthread_t *phil, int *phil_id) {
   }
 }
 
-void f_thread (void *v) {
-  int t_id = *(int *) v;
-  printf("Filósofo %d sentou-se a mesa.\n", t_id);
-  return NULL;
+
+void pensa(int phil_id) {
+  sleep(1);
+}
+
+void EsperaTaca(int phil_id) {
+  while (1) {
+    if ( phil_id == taca[0]%5) {
+      printf("filosofo %d pegou a taça ಠ_ಠ \n", phil_id);
+      temTaca[0] = phil_id;
+      break;
+    }
+    else if ( phil_id == taca[1]%5) {
+      printf("filosofo %d pegou a taça ಠ_ಠ \n", phil_id);
+      temTaca[1] = phil_id;
+      break;
+    }
+  }
+}
+
+void EsperaGarfo(int phil_id){
+  
 }
