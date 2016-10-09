948f435ea8ba522a2aa0824feb83840f7250aa89
vigkk <viglionigr@gmail.com>
2016-10-09 20:57:00 -0300

    segf

--- new_phil.c
+++ new_phil.c
@@ -19,15 +19,27 @@ int ref_comidas[FIL] = {0,0,0,0,0};
 volatile int taca[2] = {0,1};
 volatile int temTaca[2] = {-1,-1};
 volatile int temGarfo[5] = {-1,-1,-1,-1,-1};
-
+volatile int terceiro = 0;
 
 // cabeçalhos
+
+void imprime() {
+  int i;
+  printf("[");
+  for (i=0; i<5;i++)
+    printf("%d, ",temGarfo[i]);
+  puts("]");
+}
+
 
 void create_phils(pthread_t *phil, int *phil_id);
 void f_thread (void *v);
 void pensa(int phil_id);
 void EsperaTaca(int phil_id);
 void EsperaGarfo(int phil_id);
+void PegaGarfo(int phil_id);
+void PegaOutroGarfo( int phil_id);
+void PegaUnicoGarfo();
 
 // Main
 
@@ -53,7 +65,9 @@ void f_thread (void *v) {
 
   pensa(t_id);
   EsperaTaca(t_id);
-  
+  EsperaGarfo(t_id);
+  PegaOutroGarfo(t_id);
+  imprime();
   return NULL;
 }
 
@@ -88,5 +102,61 @@ void EsperaTaca(int phil_id) {
 }
 
 void EsperaGarfo(int phil_id){
-  
+  while(1) {
+    if ( phil_id == temTaca[0] || phil_id == temTaca[1]) {
+      PegaGarfo(phil_id);
+      break;
+    }
+  } 
 }
+
+void PegaGarfo (int phil_id) {
+  int i = 0;
+  while ( temGarfo[i] != -1 || i == 4)
+    i++;
+  if (temGarfo[i] == -1) {
+    temGarfo[i] = phil_id;
+    printf("filosofo %d pegou um garfo ಠ_ಠ \n", phil_id);
+  }
+  else
+    exit(1);
+}
+
+void PegaOutroGarfo( int phil_id) {
+   
+
+  while (terceiro == 0)
+    PegaUnicoGarfo(); // 3o filosofo pega um garfo
+
+  int i = 0;
+  while ( temGarfo[i] != -1 || i == 4)
+    i++;
+  if (temGarfo[i] == -1) {
+    temGarfo[i] = phil_id;
+    printf("filosofo %d pegou outro garfo ಠ_ಠ \n", phil_id);
+  }
+  else
+    exit(1);
+
+
+}
+
+void PegaUnicoGarfo() {
+  int counter = 0, i, owner[2]={-1,-2},j=0;
+  
+  for (i=0;i<5;i++)
+    if ( temGarfo[i] != -1) {
+      counter++;
+      if (owner[j] != owner[(j+1)%2]) {
+	owner[j] = temGarfo[i];
+	j++;
+      }
+    }
+
+  if (counter == 2) {
+    PegaGarfo((13*(owner[0]+owner[1]))%5);
+    terceiro = 1;
+  }
+
+}
+
