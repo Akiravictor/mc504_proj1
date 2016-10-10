315ab59597959fe43ff951136248991746bde2f9
vigkk <viglionigr@gmail.com>
2016-10-10 20:12:07 -0300

    deus eh topster++

--- new_phil.c
+++ new_phil.c
@@ -12,21 +12,23 @@
 
 // definiçoes
 
-#define REF 50 // número de refeiçoes servidas
+#define REF 15 // número de refeiçoes servidas
 #define FIL 5 // número de filósofos
 
 
 // variáveis globais
 
-int ref_comidas[FIL] = {0,0,0,0,0};               // contador de refeiçoes
+volatile int ref_comidas[FIL];               // contador de refeiçoes
 volatile int taca[2];                            // define quem começa com as taças
 volatile int temTaca[2] = {-1,-1};              // armazena quem tem as taças em dado momento    
-volatile int temGarfo[5] = {-1,-1,-1,-1,-1};   // idem sobre os garfos
-volatile int terceiro = 0;                    // verifica que um 3o filosofo pegue um garfo  
+volatile int temGarfo[FIL];   // idem sobre os garfos
+volatile int terceiro = -1;                    // verifica que um 3o filosofo pegue um garfo  
 volatile int ref = 0;                        // conta as refeiçoes feitas  
-
+volatile char estado[FIL];
 
 // cabeçalhos
+void coolprint(char i);
+void MostraEstado();
 void imprime();                                          // imprime o numero de refeiçoes feitas
 void create_phils(pthread_t *phil, int *phil_id);       // cria as threads
 void f_thread (void *v);                               // 
@@ -48,9 +50,15 @@ int main(){
   pthread_t phil[FIL];                         
   int r, i, phil_id[FIL];
 
-  r = random()%5;
+  for(i=0;i<FIL;i++) {
+    temGarfo[i]=-1;
+    ref_comidas[i] = 0;
+    estado[i] = 'z';
+  }
+
+  r = random()%FIL;
   taca[0] = r;
-  taca[1] = (r+1)%5;
+  taca[1] = (r+1)%FIL;
   
   create_phils(phil,phil_id);
   
@@ -64,15 +72,21 @@ int main(){
 
 void f_thread (void *v) {
   int t_id = *(int *) v;
-  printf("Filósofo %d sentou-se a mesa. ʘ‿ʘ \n", t_id);
-
+  //printf("Filósofo %d sentou-se a mesa. ʘ‿ʘ \n", t_id);
+  
   while (1) {
     pensa(t_id);
+    MostraEstado();
     EsperaTaca(t_id);
+    MostraEstado();
     EsperaGarfo(t_id);
+    MostraEstado();
     PegaUnicoGarfo(t_id);
+    MostraEstado();
     PegaOutroGarfo(t_id);
+    MostraEstado();
     Come(t_id);
+    MostraEstado();
     SoltaTalher(t_id);
   }
   return NULL;
@@ -90,40 +104,45 @@ void create_phils(pthread_t *phil, int *phil_id) {
 
 
 void pensa(int phil_id) {
+  estado[phil_id]='g';
+  puts("");
   sleep(1);
 }
 
 void EsperaTaca(int phil_id) {
+  estado[phil_id]='a';
   while (1) {
 
-    if ( phil_id == taca[0]%5) {
-      printf("filosofo %d pegou a taça ಠ_ಠ \n", phil_id);
+    if ( phil_id == taca[0]%FIL) {
+      //printf("filosofo %d pegou a taça \t\t ಠ_ಠ φ\n", phil_id);
       temTaca[0] = phil_id;
       break;
     }
-    else if ( phil_id == taca[1]%5) {
-      printf("filosofo %d pegou a taça ಠ_ಠ \n", phil_id);
+    else if ( phil_id == taca[1]%FIL) {
+      //printf("filosofo %d pegou a taça \t\t ಠ_ಠ φ\n", phil_id);
       temTaca[1] = phil_id;
       break;
     }
   }
+  estado[phil_id]='b';
 }
 
 void EsperaGarfo(int phil_id){
-      
+  
   while(1) {
  
     if ( phil_id == temTaca[0] || phil_id == temTaca[1]) {
       PegaGarfo(phil_id);
       break;
     }
-  } 
+  }
+  estado[phil_id]='c';
 }
 
 void PegaGarfo (int phil_id) {
   int j, i = 0;
 
-  for (j=0;j<5;j++)
+  for (j=0;j<FIL;j++)
     if ( phil_id == temGarfo[j])
       return;
   
@@ -131,45 +150,46 @@ void PegaGarfo (int phil_id) {
     i++;
   if (temGarfo[i] == -1) {
     temGarfo[i] = phil_id;
-    printf("filosofo %d pegou um garfo ಠ_ಠ \n", phil_id);
+    //printf("filosofo %d pegou um garfo \t\t ಠ_ಠ \n", phil_id);
   }
   else
     exit(1);
 }
 
 void PegaOutroGarfo( int phil_id) {
+
   int i = 0;
   while ( temGarfo[i] != -1 && i <= 4)
     i++;
-  printf("%d\n",i);
   if (temGarfo[i] == -1) {
     temGarfo[i] = phil_id;
-    printf("filosofo %d pegou outro garfo ಠ_ಠ \n", phil_id);
+    //printf("filosofo %d pegou outro garfo \t\t ಠ_ಠ \n", phil_id);
   }
   else
     exit(1);
+  estado[phil_id]='d';
 }
 
 void PegaUnicoGarfo(int phil_id) {
-
-  if (phil_id%2 == 0 && terceiro == 0) {
-    int counter = 0, i, nowner=(phil_id+2)%5;
-    for(i=0;i<5;i++) {
+  estado[phil_id]='e';
+  if (phil_id%2 == 0 && terceiro == -1) {
+    int counter = 0, i, nowner=2011*(phil_id+FIL+1)%FIL;
+    for(i=0;i<FIL;i++) {
       if(temGarfo[i] != -1) {
 	counter++;
       }
     }
   
-    for(i=0;i<5;i++) {
+    for(i=0;i<FIL;i++) {
       if(nowner == temGarfo[i]) {
-	nowner=(nowner+1)%5;
+	nowner=(nowner+1)%FIL;
 	i=0;
       }
     }
 
-    if (counter < 5) { 
+    if (counter < FIL) { 
       PegaGarfo(nowner);
-      terceiro = 1;
+      terceiro = nowner;
     }
   }
 
@@ -177,11 +197,12 @@ void PegaUnicoGarfo(int phil_id) {
 
 void Come(int phil_id) {
   int i, counter = 0;
-  for (i=0;i<5;i++)
+  for (i=0;i<FIL;i++)
     if (phil_id == temGarfo[i])
       counter++;
   if (counter == 2) {
-    printf("filósofo %d comeu ( ͡° ͜ʖ ͡°) \n", phil_id);
+    //printf("filósofo %d comeu \t\t ( ͡° ͜ʖ ͡°) \n", phil_id);
+    estado[phil_id]='f';
     ref_comidas[phil_id]++;
     ref++;
     if (ref == REF) {
@@ -196,28 +217,80 @@ void SoltaTalher (int phil_id) {
   
   if (temTaca[0] == phil_id) {
     temTaca[0] = -1;
-    taca[0]++;
+       taca[0]++;
   }
   else if (temTaca[1] == phil_id) {
     temTaca[1] = -1;
-    taca[1];
+        taca[1]++;
   }
 
-  for(i=0;i<5;i++) {
+  for(i=0;i<FIL;i++) {
     if (temGarfo[i] == phil_id) {
       temGarfo[i] = -1;
       counter++;
     }
   }
 
-  
+  taca[0] = terceiro;
+  terceiro=-1;
+
   if (counter != 2)
     exit(1);
+
 }
 
 void imprime() {
   int i;
   puts("");
-  for (i=0; i<5;i++)
+  for (i=0; i<FIL;i++)
     printf("filósofo %d comeu %d vezes\n",i,ref_comidas[i]);
+}
+
+
+void MostraEstado() {
+  int i;
+  puts("");
+  puts("");
+  for(i=0;i<FIL;i++) {
+    printf("F%d\t\t|",i);
+  }
+  puts("");
+  for(i=0;i<FIL;i++) {
+    coolprint(estado[i]);
+  }
+
+}
+
+void coolprint(char i) {
+  switch(i){
+  case 'a': {
+    printf("ಠ╭╮ಠ\t\t|");
+    break;
+  }
+  case 'b': {
+    printf("ಠ_ಠ Y\t\t|");
+    break;
+  }
+  case 'c': {
+    printf("ಠ_ಠ ψY\t\t|");
+    break;
+  }
+  case 'd': {
+    printf("ψ ಠ_ಠ ψY\t|");
+    break;
+  }
+  case 'e': {
+    printf("ಠ_ಠ ψ\t\t|");
+    break;
+  }
+  case 'f': {
+    printf("( ͡° ͜ʖ ͡°)\t|");
+    break;
+  }
+  case 'g': {
+    printf("(˘⌣˘)\t\t|");
+    break;
+  }
+  
+  }
 }
